58c72c71ab933f2336e5014043f7259f0b1de927
davidsantander9@gmail.com <davidsantander9@gmail.com>
2023-10-11 11:26:25 -0600

    Create a cell

--- TableViews/ViewController.swift
+++ TableViews/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    private let myCountries = ["Spain", "Mexico", "Peru", "Colombia", "Argentina"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
     }
 
 
+}
+
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myCountries.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "myCell")
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: "myCell")
+            cell?.backgroundColor = .lightGray
+            cell?.textLabel?.font = UIFont.systemFont(ofSize: 18)
+        }
+        cell?.textLabel?.text = myCountries[indexPath.row]
+        return cell!
+    }
+    
+    
 }
 
